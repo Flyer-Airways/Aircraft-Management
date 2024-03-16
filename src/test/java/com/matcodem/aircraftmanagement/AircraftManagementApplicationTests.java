b3b012c21753f4e4ba35c2abc7228703afc22c38
Mateusz <mateuszmilewczyk1@gmail.com>
2024-03-16 21:46:34 +0100

    init project

---
+++ src/test/java/com/matcodem/aircraftmanagement/AircraftManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matcodem.aircraftmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
